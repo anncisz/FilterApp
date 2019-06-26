1aae9c0ce4a1eee210e1013784028d5a3612d3ff
anncisz <anncisz@gmail.com>
2019-06-26 18:37:37 +0200

    first commit

---
+++ PAMProject/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace PAMProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.AddImg = new System.Windows.Forms.Button();
+            this.imgBox = new System.Windows.Forms.PictureBox();
+            this.delImg = new System.Windows.Forms.Button();
+            this.sepiaBtn = new System.Windows.Forms.Button();
+            this.saveImg = new System.Windows.Forms.Button();
+            this.bwBtn = new System.Windows.Forms.Button();
+            this.negBtn = new System.Windows.Forms.Button();
+            this.delfilterBtn = new System.Windows.Forms.Button();
+            this.grnscrBtn = new System.Windows.Forms.Button();
+            this.trcThreshold = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblContrastValue = new System.Windows.Forms.Label();
+            this.trcBrightness = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblBrightnessValue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcThreshold)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcBrightness)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddImg
+            // 
+            this.AddImg.Location = new System.Drawing.Point(9, 10);
+            this.AddImg.Margin = new System.Windows.Forms.Padding(2);
+            this.AddImg.Name = "AddImg";
+            this.AddImg.Size = new System.Drawing.Size(83, 27);
+            this.AddImg.TabIndex = 0;
+            this.AddImg.Text = "Add image";
+            this.AddImg.UseVisualStyleBackColor = true;
+            this.AddImg.Click += new System.EventHandler(this.addImg_Click);
+            // 
+            // imgBox
+            // 
+            this.imgBox.Location = new System.Drawing.Point(97, 10);
+            this.imgBox.Margin = new System.Windows.Forms.Padding(2);
+            this.imgBox.Name = "imgBox";
+            this.imgBox.Size = new System.Drawing.Size(767, 455);
+            this.imgBox.TabIndex = 1;
+            this.imgBox.TabStop = false;
+            // 
+            // delImg
+            // 
+            this.delImg.Location = new System.Drawing.Point(9, 49);
+            this.delImg.Margin = new System.Windows.Forms.Padding(2);
+            this.delImg.Name = "delImg";
+            this.delImg.Size = new System.Drawing.Size(83, 28);
+            this.delImg.TabIndex = 2;
+            this.delImg.Text = "Delete image";
+            this.delImg.UseVisualStyleBackColor = true;
+            this.delImg.Click += new System.EventHandler(this.delImg_Click);
+            // 
+            // sepiaBtn
+            // 
+            this.sepiaBtn.Location = new System.Drawing.Point(9, 475);
+            this.sepiaBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.sepiaBtn.Name = "sepiaBtn";
+            this.sepiaBtn.Size = new System.Drawing.Size(80, 27);
+            this.sepiaBtn.TabIndex = 3;
+            this.sepiaBtn.Text = "Sepia";
+            this.sepiaBtn.UseVisualStyleBackColor = true;
+            this.sepiaBtn.Click += new System.EventHandler(this.sepiaBtn_Click);
+            // 
+            // saveImg
+            // 
+            this.saveImg.Location = new System.Drawing.Point(9, 90);
+            this.saveImg.Margin = new System.Windows.Forms.Padding(2);
+            this.saveImg.Name = "saveImg";
+            this.saveImg.Size = new System.Drawing.Size(83, 27);
+            this.saveImg.TabIndex = 4;
+            this.saveImg.Text = "Save image";
+            this.saveImg.UseVisualStyleBackColor = true;
+            this.saveImg.Click += new System.EventHandler(this.saveImg_Click);
+            // 
+            // bwBtn
+            // 
+            this.bwBtn.Location = new System.Drawing.Point(93, 475);
+            this.bwBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.bwBtn.Name = "bwBtn";
+            this.bwBtn.Size = new System.Drawing.Size(80, 27);
+            this.bwBtn.TabIndex = 5;
+            this.bwBtn.Text = "Black\'n\'White";
+            this.bwBtn.UseVisualStyleBackColor = true;
+            this.bwBtn.Click += new System.EventHandler(this.bwBtn_Click);
+            // 
+            // negBtn
+            // 
+            this.negBtn.Location = new System.Drawing.Point(177, 476);
+            this.negBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.negBtn.Name = "negBtn";
+            this.negBtn.Size = new System.Drawing.Size(82, 26);
+            this.negBtn.TabIndex = 6;
+            this.negBtn.Text = "Negative";
+            this.negBtn.UseVisualStyleBackColor = true;
+            this.negBtn.Click += new System.EventHandler(this.negBtn_Click);
+            // 
+            // delfilterBtn
+            // 
+            this.delfilterBtn.Location = new System.Drawing.Point(9, 437);
+            this.delfilterBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.delfilterBtn.Name = "delfilterBtn";
+            this.delfilterBtn.Size = new System.Drawing.Size(80, 28);
+            this.delfilterBtn.TabIndex = 7;
+            this.delfilterBtn.Text = "No filter";
+            this.delfilterBtn.UseVisualStyleBackColor = true;
+            this.delfilterBtn.Click += new System.EventHandler(this.delfilterBtn_Click);
+            // 
+            // grnscrBtn
+            // 
+            this.grnscrBtn.Location = new System.Drawing.Point(263, 474);
+            this.grnscrBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.grnscrBtn.Name = "grnscrBtn";
+            this.grnscrBtn.Size = new System.Drawing.Size(88, 27);
+            this.grnscrBtn.TabIndex = 8;
+            this.grnscrBtn.Text = "ChromaKey\'d";
+            this.grnscrBtn.UseVisualStyleBackColor = true;
+            this.grnscrBtn.Click += new System.EventHandler(this.grnscrBtn_Click);
+            // 
+            // trcThreshold
+            // 
+            this.trcThreshold.AutoSize = false;
+            this.trcThreshold.BackColor = System.Drawing.Color.LightGray;
+            this.trcThreshold.Location = new System.Drawing.Point(12, 524);
+            this.trcThreshold.Maximum = 100;
+            this.trcThreshold.Minimum = -100;
+            this.trcThreshold.Name = "trcThreshold";
+            this.trcThreshold.Size = new System.Drawing.Size(314, 23);
+            this.trcThreshold.TabIndex = 24;
+            this.trcThreshold.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trcThreshold.ValueChanged += new System.EventHandler(this.TrcThreshold_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 508);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Contrast";
+            // 
+            // lblContrastValue
+            // 
+            this.lblContrastValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContrastValue.Location = new System.Drawing.Point(332, 520);
+            this.lblContrastValue.Name = "lblContrastValue";
+            this.lblContrastValue.Size = new System.Drawing.Size(57, 30);
+            this.lblContrastValue.TabIndex = 26;
+            this.lblContrastValue.Text = "0";
+            this.lblContrastValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trcBrightness
+            // 
+            this.trcBrightness.AutoSize = false;
+            this.trcBrightness.BackColor = System.Drawing.Color.LightGray;
+            this.trcBrightness.Location = new System.Drawing.Point(395, 524);
+            this.trcBrightness.Maximum = 100;
+            this.trcBrightness.Minimum = -100;
+            this.trcBrightness.Name = "trcBrightness";
+            this.trcBrightness.Size = new System.Drawing.Size(314, 23);
+            this.trcBrightness.TabIndex = 27;
+            this.trcBrightness.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trcBrightness.ValueChanged += new System.EventHandler(this.trcBrightness_valueCHanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(395, 508);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Brightness";
+            // 
+            // lblBrightnessValue
+            // 
+            this.lblBrightnessValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBrightnessValue.Location = new System.Drawing.Point(715, 517);
+            this.lblBrightnessValue.Name = "lblBrightnessValue";
+            this.lblBrightnessValue.Size = new System.Drawing.Size(57, 30);
+            this.lblBrightnessValue.TabIndex = 29;
+            this.lblBrightnessValue.Text = "0";
+            this.lblBrightnessValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 559);
+            this.Controls.Add(this.lblBrightnessValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trcBrightness);
+            this.Controls.Add(this.lblContrastValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trcThreshold);
+            this.Controls.Add(this.grnscrBtn);
+            this.Controls.Add(this.delfilterBtn);
+            this.Controls.Add(this.negBtn);
+            this.Controls.Add(this.bwBtn);
+            this.Controls.Add(this.saveImg);
+            this.Controls.Add(this.sepiaBtn);
+            this.Controls.Add(this.delImg);
+            this.Controls.Add(this.imgBox);
+            this.Controls.Add(this.AddImg);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "FilterMaker";
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcThreshold)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcBrightness)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddImg;
+        private System.Windows.Forms.PictureBox imgBox;
+        private System.Windows.Forms.Button delImg;
+        private System.Windows.Forms.Button sepiaBtn;
+        private System.Windows.Forms.Button saveImg;
+        private System.Windows.Forms.Button bwBtn;
+        private System.Windows.Forms.Button negBtn;
+        private System.Windows.Forms.Button delfilterBtn;
+        private System.Windows.Forms.Button grnscrBtn;
+        private System.Windows.Forms.TrackBar trcThreshold;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblContrastValue;
+        private System.Windows.Forms.TrackBar trcBrightness;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblBrightnessValue;
+    }
+}
+
---
+++ PAMProject/Form1.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PAMProject
+{
+    public partial class Form1 : Form
+    {
+        Bitmap temp;
+        Bitmap contrastTemp;
+        bool isFilter, isContrast;
+        Bitmap brightnessTemp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            if(imgBox.Image == null)
+            {
+                //block if noimage
+                delImg.Enabled = false;
+                saveImg.Enabled = false;
+                delfilterBtn.Enabled = false;
+                sepiaBtn.Enabled = false;
+                bwBtn.Enabled = false;
+                negBtn.Enabled = false;
+                grnscrBtn.Enabled = false;
+                trcThreshold.Enabled = false;
+                trcBrightness.Enabled = false;
+            }
+        }
+
+        private void addImg_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog add = new OpenFileDialog();
+            add.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *png)|*.jpg; *.jpeg; *.gif; *.bmp; *png";
+            if (add.ShowDialog() == DialogResult.OK)
+            { 
+                this.imgBox.SizeMode = PictureBoxSizeMode.Zoom;
+                imgBox.Image = new Bitmap(add.FileName);             
+            }
+
+            //unblock if image
+            delImg.Enabled = true;
+            saveImg.Enabled = true;
+            delfilterBtn.Enabled = true;
+            sepiaBtn.Enabled = true;
+            bwBtn.Enabled = true;
+            negBtn.Enabled = true;
+            grnscrBtn.Enabled = true;
+            trcThreshold.Enabled = true;
+            trcBrightness.Enabled = true;
+
+            isFilter = false;
+            isContrast = false;
+
+            temp = new Bitmap(imgBox.Image);
+            contrastTemp = null;
+            brightnessTemp = null;
+        }
+
+        private void delImg_Click(object sender, EventArgs e)
+        {
+            if (imgBox.Image != null)
+            {
+                imgBox.Image = null;
+                temp = null;
+
+                //block again
+                delImg.Enabled = false;
+                saveImg.Enabled = false;
+                delfilterBtn.Enabled = false;
+                sepiaBtn.Enabled = false;
+                bwBtn.Enabled = false;
+                negBtn.Enabled = false;
+                grnscrBtn.Enabled = false;
+                trcThreshold.Enabled = false;
+                trcBrightness.Enabled = false;
+
+            }
+        }
+
+        private void sepiaBtn_Click(object sender, EventArgs e)
+        {
+
+            Bitmap bmp = new Bitmap(imgBox.Image);
+            Color p;
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    p = bmp.GetPixel(x, y);
+
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+
+                    //temp value
+                    int tr = (int)(0.393 * r + 0.769 * g + 0.189 * b);
+                    int tg = (int)(0.349 * r + 0.686 * g + 0.168 * b);
+                    int tb = (int)(0.272 * r + 0.534 * g + 0.131 * b);
+
+                    //new value
+                    if (tr > 255)
+                    {
+                        r = 255;
+                    }
+                    else
+                    {
+                        r = tr;
+                    }
+
+                    if (tg > 255)
+                    {
+                        g = 255;
+                    }
+                    else
+                    {
+                        g = tg;
+                    }
+
+                    if (tb > 255)
+                    {
+                        b = 255;
+                    }
+                    else
+                    {
+                        b = tb;
+                    }
+
+                    bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                }
+            }
+            imgBox.Image = bmp;
+            isFilter = true;
+            contrastTemp = new Bitmap(imgBox.Image);
+        }
+
+        private void saveImg_Click(object sender, EventArgs e)
+        {
+            ApplyFilter(false);
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "JPG(*.JPG)|*.jpg|JPEG(*.JPEG)|*.jpeg|GIF(*.GIF)|*.gif|BMP(*.BMP)|*.bmp|PNG(*.PNG)|*.png";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                imgBox.Image.Save(save.FileName);
+            }
+        }
+
+        private void bwBtn_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(imgBox.Image);
+            Color p;
+            int width = bmp.Width;
+            int height = bmp.Height;
+            int rgb;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    p = bmp.GetPixel(x, y);
+                    rgb = (int)Math.Round(.299 * p.R + .587 * p.G + .114 * p.B);
+                    bmp.SetPixel(x, y, Color.FromArgb(rgb, rgb, rgb));
+                }
+            }
+            imgBox.Image = bmp;
+            isFilter = true;
+            contrastTemp = new Bitmap(imgBox.Image);
+        }
+
+        private void negBtn_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(imgBox.Image);
+            Color p;
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    p = bmp.GetPixel(x, y);
+
+                    //set argb value p
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+
+                    //neg
+                    r = 255 - r;
+                    g = 255 - g;
+                    b = 255 - b;
+
+                    bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                }
+            }
+            imgBox.Image = bmp;
+            isFilter = true;
+            contrastTemp = new Bitmap(imgBox.Image);
+
+        }
+
+        private void delfilterBtn_Click(object sender, EventArgs e)
+        {
+            imgBox.Image = temp;
+            isFilter = false;
+            isContrast = false;
+            trcThreshold.Value = 0;
+            lblContrastValue.Text = trcThreshold.Value.ToString();
+            ApplyFilter(true);
+            trcBrightness.Value = 0;
+            lblBrightnessValue.Text = trcBrightness.Value.ToString();
+        }
+
+        private void grnscrBtn_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(imgBox.Image);
+
+            // Creating Key Pixel
+            int key_R = 0;
+            int key_G = 128;
+            int key_B = 0;
+
+            Color Key = new Color();
+            Key = Color.FromArgb(key_R, key_G, key_B);
+
+            double KCb = -(0.168736 * Key.R) - (0.331264 * Key.G) + (0.5 * Key.B);
+            double KCr = 0.5 * Key.R - 0.418688 * Key.G - 0.081312 * Key.B;
+
+            int height = bmp.Height;
+            int width = bmp.Width;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+
+                    double Pb = -0.168736 * c.R - 0.331264 * c.G + 0.5 * c.B;
+                    double Pr = 0.5 * c.R - 0.418688 * c.G - 0.081312 * c.B;
+
+                    // distance n function point
+                    double dist = Math.Sqrt((KCb - Pb) * (KCb - Pb) + (KCr - Pr) * (KCr - Pr));
+                    double point = 1;
+
+                    // Range
+                    int a1 = 40;
+                    int a2 = 60;
+
+                    if (dist <= a1)
+                    {
+                        point = 0;
+                    }
+                    else if (dist >= a2)
+                    {
+                        point = 1;
+                    }
+                    else if (dist > a1 && dist < a2)
+                    {
+                        point = (dist - a1) / (a2 - a1);
+                    }
+
+                    double q = 1 - point;
+
+                    double PwR = Math.Min(Math.Max(c.R - q * Key.R, 0) + q * Color.White.R, 255);
+                    double PwG = Math.Min(Math.Max(c.G - q * Key.G, 0) + q * Color.White.G, 255);
+                    double PwB = Math.Min(Math.Max(c.B - q * Key.B, 0) + q * Color.White.B, 255);
+
+                    Color FColor = new Color();
+                    FColor = Color.FromArgb((int)PwR, (int)PwG, (int)PwB);
+
+                    bmp.SetPixel(x, y, FColor);
+                }
+            }
+            imgBox.Image = bmp;
+            isFilter = true;
+            contrastTemp = new Bitmap(imgBox.Image);
+        }
+
+        private void TrcThreshold_ValueChanged(object sender, EventArgs e)
+        {
+            lblContrastValue.Text = trcThreshold.Value.ToString();
+
+            ApplyFilter(true);
+        }
+
+        private void ApplyFilter(bool preview)
+        {
+            Bitmap bmp = new Bitmap(imgBox.Image);
+            Bitmap previewBitmap = bmp;
+
+            if (bmp == null)
+            {
+                return;
+            }
+
+            if (preview == true)
+            {
+                if (isFilter == true)
+                {
+                    previewBitmap = contrastTemp;
+                    imgBox.Image = previewBitmap.Contrast(trcThreshold.Value);
+                    isContrast = true;
+                    brightnessTemp = new Bitmap(imgBox.Image);
+                }
+                else
+                {
+                    previewBitmap = temp;
+                    imgBox.Image = previewBitmap.Contrast(trcThreshold.Value);
+                    isContrast = true;
+                    brightnessTemp = new Bitmap(imgBox.Image);
+                }
+            }
+            else
+            {
+                imgBox.Image = bmp.Contrast(trcThreshold.Value);
+            }
+
+        }
+
+        private void trcBrightness_valueCHanged(object sender, EventArgs e)
+        {
+            lblBrightnessValue.Text = trcBrightness.Value.ToString();
+
+            AdjustBrightness(new Bitmap(imgBox.Image), trcBrightness.Value);
+        }
+
+        public void AdjustBrightness(Bitmap image, int value)
+        {
+            Bitmap previewBitmap = image;
+            if (isFilter == true)
+            {
+                previewBitmap = contrastTemp;
+            }
+            if(isContrast == true)
+            {
+                previewBitmap = brightnessTemp;
+            } else 
+            {
+                previewBitmap = temp;
+            }
+                //System.Drawing.Bitmap TempBitmap = image;
+            float FinalValue = (float)value / 255.0f;
+            System.Drawing.Bitmap NewBitmap = new System.Drawing.Bitmap(previewBitmap.Width, previewBitmap.Height);
+            System.Drawing.Graphics NewGraphics = System.Drawing.Graphics.FromImage(NewBitmap);
+            float[][] FloatColorMatrix ={
+                    new float[] {1, 0, 0, 0, 0},
+                    new float[] {0, 1, 0, 0, 0},
+                    new float[] {0, 0, 1, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {FinalValue, FinalValue, FinalValue, 1, 1}
+                };
+
+            System.Drawing.Imaging.ColorMatrix NewColorMatrix = new System.Drawing.Imaging.ColorMatrix(FloatColorMatrix);
+            System.Drawing.Imaging.ImageAttributes Attributes = new System.Drawing.Imaging.ImageAttributes();
+            Attributes.SetColorMatrix(NewColorMatrix);
+            NewGraphics.DrawImage(previewBitmap, new System.Drawing.Rectangle(0, 0, previewBitmap.Width, previewBitmap.Height), 0, 0, previewBitmap.Width, previewBitmap.Height, System.Drawing.GraphicsUnit.Pixel, Attributes);
+            Attributes.Dispose();
+            NewGraphics.Dispose();
+            imgBox.Image = NewBitmap;
+        }
+    }
+}
